16f748ac058bf5048f4fe3a925c1b911ac244a1f
Avinash sharma <sharmaavi2196@gmail.com>
2025-06-17 23:24:43 +0530

    Add recursive functions printNumber and sum to demonstrate recursion

---
+++ Recursion.js
@@ -0,0 +1,19 @@
+function printNumber(n) {
+  if (n < 1) {
+    return;
+  }
+  printNumber(n - 1);
+  console.log(n);
+}
+
+printNumber(10);
+
+function sum(n) {
+  if (n == 0) {
+    return 0;
+  }
+  console.log(n);
+  return n + sum(n - 1);
+}
+
+sum(15);
