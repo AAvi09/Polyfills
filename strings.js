62e6ad2d3b2b2063aa65697739e2e27923546278
Avinash sharma <sharmaavi2196@gmail.com>
2025-07-08 00:45:58 +0530

    Add lengthOfLastWord function to calculate the length of the last word in a string

---
+++ strings.js
@@ -0,0 +1,12 @@
+function lengthOfLastWord(s) {
+  let count = 0;
+  for (let i = s.length - 1; i >= 0; i--) {
+    if (s[i] === " " && count > 0) {
+      break;
+    }
+    if (s[i] !== " ") {
+      count++;
+    }
+  }
+  return count;
+}
