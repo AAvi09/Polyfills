985e084f23ec967c50bf06954b55ba7f4eb071e8
Avinash sharma <sharmaavi2196@gmail.com>
2025-07-09 20:52:52 +0530

    Add isPalindrome function to check if a string is a palindrome

--- strings.js
+++ strings.js
@@ -72,3 +72,21 @@ var reverseStr = function (s, k) {
   }
   return s.join("");
 };
+
+var isPalindrome = function (s) {
+  s = s.toLowerCase();
+  let i = 0;
+  let j = s.length - 1;
+  while (i < j) {
+    if (!s[i].match(/[a-z0-9]/i)) {
+      i++;
+    } else if (!s[j].match(/[a-z0-9]/)) {
+      j--;
+    } else if (s[i] === s[j]) {
+      i++, j--;
+    } else {
+      return false;
+    }
+  }
+  return true;
+};
