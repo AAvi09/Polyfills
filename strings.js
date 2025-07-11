192c2dcfd3ce956927707f3fb2504287a2d85171
Avinash sharma <sharmaavi2196@gmail.com>
2025-07-11 23:54:59 +0530

    Add largestCommonPrefix function to find the longest common prefix among an array of strings

--- strings.js
+++ strings.js
@@ -99,3 +99,12 @@ function largestOddNumber(num) {
   }
   return "";
 }
+
+function largestCommonPrefix(strs) {
+  let x = 0;
+  for (let i = 0; i < strs[0].length; i++) {
+    for (let j = 1; j < strs.length; j++) {
+      if (strs[0][i] !== strs[j][i]) return strs[0].slice(0, i);
+    }
+  }
+}
