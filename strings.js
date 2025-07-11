977d8b03b6d3c03ed831ac194bcc3912f7f08f03
Avinash sharma <sharmaavi2196@gmail.com>
2025-07-11 23:59:33 +0530

    Remove unused variable in largestCommonPrefix function

--- strings.js
+++ strings.js
@@ -101,7 +101,6 @@ function largestOddNumber(num) {
 }
 
 function largestCommonPrefix(strs) {
-  let x = 0;
   for (let i = 0; i < strs[0].length; i++) {
     for (let j = 1; j < strs.length; j++) {
       if (strs[0][i] !== strs[j][i]) return strs[0].slice(0, i);
