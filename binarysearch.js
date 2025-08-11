20afad2b20fa07a108189b498d29c3d8e69ca95c
Avinash sharma <sharmaavi2196@gmail.com>
2025-08-12 00:01:25 +0530

    peak element

--- binarysearch.js
+++ binarysearch.js
@@ -200,3 +200,19 @@ var nextGreatestLetter = function (letters, target) {
   }
   return target < letters[l] ? letters[l] : letters[0];
 };
+
+var peakIndexInMountainArray = function (arr) {
+  let l = 0;
+  let r = arr.length - 1;
+  while (l < r) {
+    let m = Math.floor(l + (r - l) / 2);
+    if (arr[m] > arr[m - 1] && arr[m] > arr[m + 1]) {
+      return m;
+    } else if (arr[m] > arr[m - 1] && arr[m] < arr[m + 1]) {
+      l = m + 1;
+    } else {
+      r = m;
+    }
+  }
+  return l;
+};
