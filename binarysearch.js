aae422c2cc68af4ddd5da70224d54ac305aa48f2
Avinash sharma <sharmaavi2196@gmail.com>
2025-08-12 22:08:51 +0530

    single non duplicate using binary search

--- binarysearch.js
+++ binarysearch.js
@@ -216,3 +216,18 @@ var peakIndexInMountainArray = function (arr) {
   }
   return l;
 };
+
+var singleNonDuplicate = function (nums) {
+  let l = 0;
+  let r = nums.length - 1;
+  while (l < r) {
+    let m = Math.floor(l + (r - l) / 2);
+    if (m % 2 === 1) m--;
+    if (nums[m] === nums[m + 1]) {
+      l = m + 2;
+    } else {
+      r = m;
+    }
+  }
+  return nums[l];
+};
