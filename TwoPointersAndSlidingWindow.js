1e3529e90030a64f835e7ee0cbeb1b9175b6d92a
Avinash sharma <sharmaavi2196@gmail.com>
2025-08-18 00:03:45 +0530

    container with max water

--- TwoPointersAndSlidingWindow.js
+++ TwoPointersAndSlidingWindow.js
@@ -128,3 +128,20 @@ var getIntersectionNode = function (headA, headB) {
   }
   return pA;
 };
+
+var maxArea = function (height) {
+  let i = 0;
+  let j = height.length - 1;
+  let maxWater = 0;
+  while (i < j) {
+    let area = Math.min(height[i], height[j]) * (j - i);
+    maxWater = Math.max(maxWater, area);
+
+    if (height[i] > height[j]) {
+      j--;
+    } else {
+      i++;
+    }
+  }
+  return maxWater;
+};
