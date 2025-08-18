0c0df38cb69dbda8903e0c78c368a4585c900878
Avinash sharma <sharmaavi2196@gmail.com>
2025-08-19 00:30:01 +0530

    3sum algo changed

--- TwoPointersAndSlidingWindow.js
+++ TwoPointersAndSlidingWindow.js
@@ -151,24 +151,27 @@ var threeSum = function (arr) {
   arr.sort((a, b) => a - b);
   let ans = [];
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] != arr[i - 1]) {
+    if (i == 0 || arr[i] != arr[i - 1]) {
       twoSum(arr, i, ans);
     }
   }
   return ans;
-
-  var twoSum = function (arr, x, ans) {
-    let i = x + 1;
-    let j = arr.length - 1;
-    while (i < j) {
-      let sum = arr[i] + arr[j] + arr[x];
-      if (sum > 0) {
-        j--;
-      } else if (sum < 0) {
-        l++;
-      } else {
-        ans.push([ans[i], arr[j], arr[x]]);
-      }
+};
+var twoSum = function (arr, x, ans) {
+  let i = x + 1;
+  let j = arr.length - 1;
+  while (i < j) {
+    let sum = arr[i] + arr[j] + arr[x];
+    if (sum > 0) {
+      j--;
+    } else if (sum < 0) {
+      i++;
+    } else {
+      ans.push([arr[i], arr[j], arr[x]]);
+      i++;
+      j--;
+      while (i < j && arr[i] === arr[i - 1]) i++;
+      while (i < j && arr[j] === arr[j + 1]) j--;
     }
-  };
+  }
 };
